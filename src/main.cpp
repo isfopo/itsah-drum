5d109ee3794048230c91665b2c758dd35a59cdd8
Isaac Poole <isaacpoolemusic@gmail.com>
2021-10-19 06:29:30 -0400

    sets up shift grid

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <MIDIUSB.h>
 #include "Note.h"
 
-#define MIDI_CHANNEL     0  // default channel # is 0
+#define MIDI_CHANNEL 0 // default channel # is 0
 #define FIRST_MIDI_NOTE 36
 
 Adafruit_NeoTrellisM4 trellis = Adafruit_NeoTrellisM4();
@@ -16,67 +16,84 @@ Adafruit_ADXL343 accel = Adafruit_ADXL343(123, &Wire1);
 uint32_t tick = 0;
 
 // colors
-uint32_t press_color = 0XFFFFFF;
-uint32_t on_color = 0X00FF00;
+uint32_t column_color = 0XFFFFFF;
+uint32_t shift_column_color = 0X00FFFF;
+uint32_t main_color = 0X00FF00;
+uint32_t shift_color = 0x0000FF;
 uint32_t off_color = 0X0;
 
 const int NUMBER_OF_COLUMNS = 8;
 const int NUMBER_OF_ROWS = 4;
 
 Note main_grid[NUMBER_OF_COLUMNS][NUMBER_OF_ROWS];
+Note shift_grid[NUMBER_OF_COLUMNS][NUMBER_OF_ROWS];
 
 boolean pressed_keys[32];
 boolean combo_pressed = false;
 
 // modes
 boolean main_mode = true;
-boolean shift_mode = false;
 
 // combos
-int back_combo[] = { 6, 20, 31 };
-int shift_combo[] = { 7, 20, 31 };
+int back_combo[] = {7, 28, 31};
+int shift_combo[] = {7, 20, 31};
 
 // floating point map
-float ofMap(float value, float inputMin, float inputMax, float outputMin, float outputMax, bool clamp) {
-    float outVal = ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
+float ofMap(float value, float inputMin, float inputMax, float outputMin, float outputMax, bool clamp)
+{
+  float outVal = ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
 
-    if (clamp) {
-      if (outputMax < outputMin) {
-        if (outVal < outputMax)  outVal = outputMax;
-        else if (outVal > outputMin)  outVal = outputMin;
-      } else {
-        if (outVal > outputMax) outVal = outputMax;
-        else if (outVal < outputMin)  outVal = outputMin;
-      }
+  if (clamp)
+  {
+    if (outputMax < outputMin)
+    {
+      if (outVal < outputMax)
+        outVal = outputMax;
+      else if (outVal > outputMin)
+        outVal = outputMin;
     }
-    return outVal;
+    else
+    {
+      if (outVal > outputMax)
+        outVal = outputMax;
+      else if (outVal < outputMin)
+        outVal = outputMin;
+    }
+  }
+  return outVal;
 }
 
 void play(Note note)
 {
-  if (note.is_on) {
-    if (note.is_accented) 
+  if (note.is_on)
+  {
+    if (note.is_accented)
     {
       trellis.noteOn(note.midi, 127);
-    } else {
+    }
+    else
+    {
       trellis.noteOn(note.midi, 96);
     }
   }
 }
 
 void stop(Note note)
-{ 
+{
   trellis.noteOff(note.midi, 0);
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
+uint32_t Wheel(byte WheelPos)
+{
   WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
+  if (WheelPos < 85)
+  {
     return trellis.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
-  if(WheelPos < 170) {
+  if (WheelPos < 170)
+  {
     WheelPos -= 85;
     return trellis.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
@@ -84,27 +101,34 @@ uint32_t Wheel(byte WheelPos) {
   return trellis.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
-uint32_t sixteenthNoteToTicks(uint8_t sixteenthNote) {
+uint32_t sixteenthNoteToTicks(uint8_t sixteenthNote)
+{
   return sixteenthNote * 6;
 }
 
-uint32_t tickToEighthNote(uint32_t tick) {
+uint32_t tickToEighthNote(uint32_t tick)
+{
   return tick / 12;
 }
 
-int coordinatesToKey( int col, int row ) {
-  return ( row * 8 ) + col;
+int coordinatesToKey(int col, int row)
+{
+  return (row * 8) + col;
 }
 
-int getGridNote(int start, int rows, int index) {
+int getGridNote(int start, int rows, int index)
+{
   return (start + rows - 1) - index;
 }
 
-int numberOfButtonPressed( bool pressed_buttons[], int size ) {
+int numberOfButtonPressed(bool pressed_buttons[], int size)
+{
   int count = 0;
 
-  for ( int i = 0; i < size; ++i ) {
-    if (pressed_buttons[i]) {
+  for (int i = 0; i < size; ++i)
+  {
+    if (pressed_buttons[i])
+    {
       count++;
     }
   }
@@ -112,18 +136,22 @@ int numberOfButtonPressed( bool pressed_buttons[], int size ) {
   return count;
 }
 
-bool checkCombo( int combo[], int size, bool pressed_buttons[] ) {
-  for ( int i = 0; i < size; i++ ) {
-    if (!pressed_buttons[combo[i]]) {
+bool checkCombo(int combo[], int size, bool pressed_buttons[])
+{
+  for (int i = 0; i < size; i++)
+  {
+    if (!pressed_buttons[combo[i]])
+    {
       return false;
     }
   }
   return true;
 }
 
-void setup(){
+void setup()
+{
   Serial.begin(115200);
-    
+
   trellis.begin();
   trellis.setBrightness(80);
 
@@ -132,47 +160,89 @@ void setup(){
   trellis.enableUSBMIDI(true);
   trellis.setUSBMIDIchannel(MIDI_CHANNEL);
 
-  for (int i = 0; i < NUMBER_OF_COLUMNS; i++) {
-    for (int j = 0; j < NUMBER_OF_ROWS; j++) {
+  for (int i = 0; i < NUMBER_OF_COLUMNS; i++)
+  {
+    for (int j = 0; j < NUMBER_OF_ROWS; j++)
+    {
       main_grid[i][j] = Note();
       main_grid[i][j].set_note(getGridNote(FIRST_MIDI_NOTE, NUMBER_OF_ROWS, j));
       main_grid[i][j].set_key(coordinatesToKey(i, j));
     }
   }
+  for (int i = 0; i < NUMBER_OF_COLUMNS; i++)
+  {
+    for (int j = 0; j < NUMBER_OF_ROWS; j++)
+    {
+      shift_grid[i][j] = Note();
+      shift_grid[i][j].set_note(getGridNote(FIRST_MIDI_NOTE, NUMBER_OF_ROWS, j));
+      shift_grid[i][j].set_key(coordinatesToKey(i, j));
+    }
+  }
 }
 
-void loop() {
+void loop()
+{
   trellis.tick();
 
-  midiEventPacket_t midi_in =  MidiUSB.read();
+  midiEventPacket_t midi_in = MidiUSB.read();
 
-  if (midi_in.header == 3) { // transport start
+  if (midi_in.header == 3)
+  { // transport start
 
     Serial.println("start");
     tick = sixteenthNoteToTicks(midi_in.byte2); // syncs ticks to transport
-
-  } 
-  else if (midi_in.header == 11) { // transport end
+  }
+  else if (midi_in.header == 11)
+  { // transport end
 
     Serial.println("stop");
+  }
+  else if (midi_in.header == 15)
+  { // tick event - happens 24 times per quarter note
 
-  } 
-  else if (midi_in.header == 15) { // tick event - happens 24 times per quarter note
-
-    if ( tick % 12 == 0 ) {
-      for ( Note note: main_grid[tickToEighthNote(tick) % 8] ) {
+    if (tick % 12 == 0)
+    {
+      for (Note note : main_grid[tickToEighthNote(tick) % 8])
+      {
         play(note);
-        trellis.setPixelColor(note.key, press_color);
+        if (main_mode)
+        {
+          trellis.setPixelColor(note.key, column_color);
+        }
       }
-      for ( Note note: main_grid[(tickToEighthNote(tick) - 1) % 8]) {
+      for (Note note : main_grid[(tickToEighthNote(tick) - 1) % 8])
+      {
         stop(note);
-        trellis.setPixelColor(note.key, note.is_on ? on_color : off_color);
+        if (main_mode)
+        {
+          trellis.setPixelColor(note.key, note.is_on ? main_color : off_color);
+        }
+      }
+    }
+    else if (tick % 12 == 7)
+    {
+      for (Note note : shift_grid[tickToEighthNote(tick) % 8])
+      {
+        play(note);
+        if (!main_mode)
+        {
+          trellis.setPixelColor(note.key, column_color);
+        }
+      }
+      for (Note note : shift_grid[(tickToEighthNote(tick) - 1) % 8])
+      {
+        stop(note);
+        if (!main_mode)
+        {
+          trellis.setPixelColor(note.key, note.is_on ? shift_color : off_color);
+        }
       }
     }
     tick++;
   }
-  
-  else if (midi_in.header != 0) {
+
+  else if (midi_in.header != 0)
+  {
     Serial.println("message in");
     Serial.println(midi_in.header);
     Serial.println(" ");
@@ -183,39 +253,75 @@ void loop() {
     Serial.println(midi_in.byte3);
   }
 
-  while (trellis.available()){
+  while (trellis.available())
+  {
     keypadEvent e = trellis.read();
     int key = e.bit.KEY;
     int col = e.bit.COL;
     int row = e.bit.ROW;
-    
-    if (e.bit.EVENT == KEY_JUST_PRESSED) {
+
+    if (e.bit.EVENT == KEY_JUST_PRESSED)
+    {
       Serial.println(" pressed\n");
       //Serial.println(key);
       pressed_keys[key] = true;
-      if (numberOfButtonPressed(pressed_keys, sizeof(pressed_keys)) > 1) {
+      if (numberOfButtonPressed(pressed_keys, sizeof(pressed_keys)) > 1)
+      {
         combo_pressed = true;
-        if ( checkCombo( shift_combo, sizeof(shift_combo) / sizeof(shift_combo[0]), pressed_keys) ) {
+        if (checkCombo(shift_combo, sizeof(shift_combo) / sizeof(shift_combo[0]), pressed_keys))
+        {
           Serial.println("shift combo");
-          shift_mode = true;
           main_mode = false;
-        } else if ( checkCombo( back_combo, sizeof(back_combo) / sizeof(back_combo[0]), pressed_keys) ) {
-          shift_mode = true;
-          main_mode = false;
+
+          for (int i = 0; i < NUMBER_OF_COLUMNS; i++)
+          {
+            for (int j = 0; j < NUMBER_OF_ROWS; j++)
+            {
+              Serial.print(shift_grid[i][j].is_on);
+              Serial.print(" - ");
+              trellis.setPixelColor(coordinatesToKey(i, j), shift_grid[i][j].is_on ? shift_color : off_color);
+            }
+          }
+        }
+        else if (checkCombo(back_combo, sizeof(back_combo) / sizeof(back_combo[0]), pressed_keys))
+        {
+          Serial.println("back combo");
+          main_mode = true;
+          for (int i = 0; i < NUMBER_OF_COLUMNS; i++)
+          {
+            for (int j = 0; j < NUMBER_OF_ROWS; j++)
+            {
+              Serial.print(main_grid[i][j].is_on);
+              Serial.print(" - ");
+              trellis.setPixelColor(coordinatesToKey(i, j), main_grid[i][j].is_on ? main_color : off_color);
+            }
+          }
         }
       }
     }
-    else if (e.bit.EVENT == KEY_JUST_RELEASED) {
+    else if (e.bit.EVENT == KEY_JUST_RELEASED)
+    {
       Serial.println(" released\n");
 
       pressed_keys[key] = false;
 
-      if (!combo_pressed) {
-        main_grid[col][row].toggle(); // toggle note
-        trellis.setPixelColor(key, main_grid[col][row].is_on ? on_color : off_color); // toggle key light
-      } 
-      else {
-        if (numberOfButtonPressed(pressed_keys, sizeof(pressed_keys)) == 0) {
+      if (!combo_pressed)
+      {
+        if (main_mode)
+        {
+          main_grid[col][row].toggle();                                                 // toggle note
+          trellis.setPixelColor(key, main_grid[col][row].is_on ? main_color : off_color); // toggle key light
+        }
+        else
+        {
+          shift_grid[col][row].toggle();                                                    // toggle note
+          trellis.setPixelColor(key, shift_grid[col][row].is_on ? shift_color : off_color); // toggle key light
+        }
+      }
+      else
+      {
+        if (numberOfButtonPressed(pressed_keys, sizeof(pressed_keys)) == 0)
+        {
           combo_pressed = false;
         }
       }
